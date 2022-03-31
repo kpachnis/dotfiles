9c5fad2f41cb947a4a2c49532c41d790c894d35a
Konstantinos Pachnis <konstantinos@bugeffect.com>
2022-04-01 01:27:15 +0300

    Increase font size

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -71,7 +71,7 @@
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
 (add-to-list 'default-frame-alist
-             '(font . "Monospace 10"))
+             '(font . "Monospace 11"))
 
 ;; M-SPC is used to change the OS input source
 (global-unset-key (kbd "M-SPC"))
