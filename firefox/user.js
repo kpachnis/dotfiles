28b05c0e216ebeef6d2641284e7ae3b3eaba2cc6
Konstantinos Pachnis <konstantinos@bugeffect.com>
2023-08-13 10:53:31 +0300

    Import user preferences

---
+++ firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("mousewheel.with_meta.action", 0);
+user_pref("extensions.pocket.enabled", false);
+user_pref("dom.event.clipboardevents.enabled", false);
