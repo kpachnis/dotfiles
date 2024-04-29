2a6d890cbe33dc5ce67989dcb4369c31dbe6e8c9
Konstantinos Pachnis <konstantinos@bugeffect.com>
2024-04-29 22:50:14 +0300

    Move plugins setup in plugins.lua file

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -39,28 +39,4 @@ vim.opt.wildmode = { 'longest:full', 'full' }
 vim.keymap.set("n", "<c-P>",
   "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
 
-require('Comment').setup()
-
-require('nvim-treesitter.configs').setup({
-    auto_install = true,
-
-    indent = {
-        enable = true
-    },
-
-    highlight = {
-        enable = true,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-})
-
-require("catppuccin").setup({
-    integrations = {
-        treesitter = true
-    }
-})
 vim.cmd.colorscheme 'catppuccin-mocha'
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,20 +1,54 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                auto_install = true,
+
+                indent = {
+                    enable = true
+                },
+
+                highlight = {
+                    enable = true,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end
+    },
+
     {
         "numToStr/Comment.nvim",
         lazy = false,
+        config = function()
+            require('Comment').setup({})
+        end
     },
-    { "catppuccin/nvim",
+
+    {
+        "catppuccin/nvim",
         name = "catppuccin",
-        priority = 1000
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                integrations = {
+                    treesitter = true
+                }
+            })
+
+        end
     },
+
     {
         "ibhagwan/fzf-lua",
-        -- optional for icon support
-        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            -- calling `setup` is optional for customization
             require("fzf-lua").setup({})
         end
-    }
+    },
+
+    { "nvim-tree/nvim-web-devicons", lazy = true },
 }
