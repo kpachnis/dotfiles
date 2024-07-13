7de26d5a2bc6098cbeb4773c9a565deee3e1dba6
Konstantinos Pachnis <kp@bugeffect.com>
2024-07-13 10:34:21 +0300

    use Neovim colors

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,8 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 
-config.color_scheme = 'catppuccin-frappe'
-config.font = wezterm.font 'JetBrainsMono Nerd Font Mono'
+config.color_scheme = 'NvimLight'
 config.enable_kitty_keyboard = true
 
 return config
