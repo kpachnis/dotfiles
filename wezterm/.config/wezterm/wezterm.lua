396d251f5696342ac8da78a39d04219e956cf9ab
Konstantinos Pachnis <kp@bugeffect.com>
2024-07-24 20:44:14 +0300

    Add helix and wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+
+config.color_scheme = 'Jellybeans'
+config.enable_kitty_keyboard = true
+config.font_size = 12
+
+return config
