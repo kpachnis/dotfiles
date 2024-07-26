575bcf2e5d171b1eb38c855f16cee40a98bcd162
Konstantinos Pachnis <kp@bugeffect.com>
2024-07-26 14:49:47 +0300

    Change theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 
-config.color_scheme = 'NvimLight'
+config.color_scheme = 'flexoki-light'
 config.enable_kitty_keyboard = true
 config.font_size = 12
 
