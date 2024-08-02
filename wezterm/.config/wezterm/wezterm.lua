251440ed81fcbe5e548e8bdde0cf3d00ad0b37ef
Konstantinos Pachnis <kp@bugeffect.com>
2024-08-02 23:54:58 +0300

    Update wezterm config
    - Use the native macOS full-screen application support
    - Hide the tab bar when there is only a single tab

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "tokyonight_night"
 config.enable_kitty_keyboard = true
+config.hide_tab_bar_if_only_one_tab = true
+config.native_macos_fullscreen_mode = true
 config.font_size = 12
 
 return config
