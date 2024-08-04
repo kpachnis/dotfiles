e6c02ddead07b1c1457fd0ec962d5435cf8e89bd
Konstantinos Pachnis <kp@bugeffect.com>
2024-08-04 11:53:53 +0300

    Change default font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,10 +2,13 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font 'IosevkaTerm Nerd Font Mono'
+
 config.color_scheme = "tokyonight_night"
 config.enable_kitty_keyboard = true
 config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = true
-config.font_size = 12
+config.font_size = 14
 
 return config
+
