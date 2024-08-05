14c1ca7ea9b6c4be50d922ea85ee22e390aea839
Konstantinos Pachnis <kp@bugeffect.com>
2024-08-05 10:37:33 +0300

    Enable autopairs fastwrap

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -86,9 +86,13 @@ return {
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
-        config = true
+        config = true,
         -- use opts = {} for passing setup options
         -- this is equalent to setup({}) function
+        opts = {
+            fast_wrap = {}
+        }
+
     },
 
     {
