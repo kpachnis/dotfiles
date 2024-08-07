f76cfacb3b0a1e85c9e182b6f9852abbeeaa10c6
Konstantinos Pachnis <kp@bugeffect.com>
2024-08-07 15:51:36 +0300

    Use GitHub light theme

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -93,6 +93,15 @@ return {
         opts = {
             fast_wrap = {}
         }
-
     },
+
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({})
+            vim.cmd('colorscheme github_light')
+        end,
+    }
 }
